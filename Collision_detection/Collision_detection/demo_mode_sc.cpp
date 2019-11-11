5393340b607224e47f3bbbaec5bbd8b5e7fb7b8a
marinapolishchuk <arcticmo267@gmail.com>
2019-11-12 00:02:23 +0200

    moving circles added

--- Collision_detection/Collision_detection/demo_mode_sc.cpp
+++ Collision_detection/Collision_detection/demo_mode_sc.cpp
@@ -1,5 +1,43 @@
 #include "demo_mode_sc.hpp"
 
+class Ball {
+private:
+    sf::CircleShape* c;
+    float px, py;
+    float vx, vy;
+public:
+    Ball(sf::Color col, float r) : c(new sf::CircleShape) {
+        c->setRadius(r);
+        c->setFillColor(col);
+        c->setOrigin(c->getPosition().x + c->getRadius(), c->getPosition().y + c->getRadius());
+        px = c->getPosition().x;
+        py = c->getPosition().y;
+    }
+    ~Ball() {}
+    sf::CircleShape getCircle() { return *c; }
+    sf::CircleShape* getACircle() { return c; }
+    void setPos(float x, float y) {
+        px = x; py = y;
+        c->setPosition(px, py);
+    }
+    void setVelocity(float x, float y) { vx = x; vy = y; }
+    void update() {
+        px = c->getPosition().x;
+        py = c->getPosition().y;
+        px += vx;
+        py += vy;
+        c->setPosition(px, py);
+    }
+    float getRadius() {
+        return c->getRadius();
+    }
+};
+
+float rand_FloatRange(float a, float b)
+{
+    return ((b - a) * ((float)rand() / RAND_MAX)) + a;
+}
+
 DemoModeSc::DemoModeSc() { }
 
 int DemoModeSc::Run(sf::RenderWindow &App) {
@@ -7,20 +45,16 @@ int DemoModeSc::Run(sf::RenderWindow &App) {
     sf::Event event;
     bool running = true;
     
-    std::vector<sf::CircleShape> circles;
-    
+    std::vector<Ball> circles;
+
     int amount_of_circles = 10;
-    
+
     for (int i = 0; i < amount_of_circles; ++i) {
-        sf::CircleShape c;
-        c.setFillColor(sf::Color::White);
-        c.setRadius(rand() % 40 + 10);
-        c.setOrigin(c.getPosition().x + c.getRadius(), c.getPosition().y + c.getRadius());
-        c.setPosition(rand() % (int)(WINDOW_WIDTH - 2 * c.getRadius()) + c.getRadius(), rand() % (int)(WINDOW_HEIGHT - 2 * c.getRadius()) + c.getRadius());
+        Ball c(sf::Color::White, rand() % 40 + 20);
+        c.setPos(rand() % (int)(WINDOW_WIDTH - 2 * c.getRadius()) + c.getRadius(), rand() % (int)(WINDOW_HEIGHT - 2 * c.getRadius()) + c.getRadius());
+        c.setVelocity(rand_FloatRange(-0.1f, 0.1f), rand_FloatRange(-0.1f, 0.1f));
         circles.push_back(c);
     }
-    
-    
     
     while(running) {
         
@@ -31,18 +65,23 @@ int DemoModeSc::Run(sf::RenderWindow &App) {
             
             switch (event.type) {
                 case sf::Event::Closed: {
+                    for (int i = 0; i < amount_of_circles; ++i) {
+                        delete circles[i].getACircle();
+                    }
                     return (-1);
                     break;
                 }
                     
-                    
             }
             
         }
-        
+       
         App.clear(sf::Color::Black);
-        for (auto &a : circles) {
-            App.draw(a);
+        for (int i = 0; i < amount_of_circles; ++i) {
+            circles[i].update();
+        }
+        for (int i = 0; i < amount_of_circles; ++i) {
+            App.draw(circles[i].getCircle());
         }
         App.display();
         
