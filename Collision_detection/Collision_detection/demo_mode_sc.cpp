ab5966b7b16553c7b688376ed769371641eeaa71
marinapolishchuk <arcticmo267@gmail.com>
2019-11-24 19:14:34 +0200

    demonstration mode was finished

--- Collision_detection/Collision_detection/demo_mode_sc.cpp
+++ Collision_detection/Collision_detection/demo_mode_sc.cpp
@@ -1,4 +1,5 @@
 #include "demo_mode_sc.hpp"
+#include <cmath>
 
 float rand_FloatRange(float a, float b)
 {
@@ -10,16 +11,18 @@ private:
     sf::CircleShape* c;
     float px, py;
     float vx, vy;
+    float mass;
     
     void init() {
         c->setRadius(rand() % 40 + 20);
         c->setFillColor(sf::Color::White);
         c->setOrigin(c->getPosition().x + c->getRadius(), c->getPosition().y + c->getRadius());
         c->setPosition(rand() % (int)(WINDOW_WIDTH - 2 * c->getRadius()) + c->getRadius(), rand() % (int)(WINDOW_HEIGHT - 2 * c->getRadius()) + c->getRadius());
-        vx = rand_FloatRange(-0.1f, 0.1f);
-        vy = rand_FloatRange(-0.1f, 0.1f);
+        vx = rand_FloatRange(-0.08f, 0.08f);
+        vy = rand_FloatRange(-0.08f, 0.08f);
         px = c->getPosition().x;
         py = c->getPosition().y;
+        mass = c->getRadius() * 10.0f;
         
     }
 public:
@@ -32,6 +35,7 @@ public:
         py = other.py;
         vx = other.vx;
         vy = other.vy;
+        mass = other.mass;
     }
     ~Ball() { delete c; }
     
@@ -39,30 +43,72 @@ public:
         window.draw(*c, states);
     }
     
-    //getters
-    sf::CircleShape* getCircle() { return c; }
-    float getRadius() { return c->getRadius(); }
-    
     //update
-    void update() {
+    void update(std::vector<Ball> &circles, sf::Int32 dt) {
         px += vx;
         py += vy;
         c->setPosition(px, py);
-        
-        // no colliding with window borders
-//        if (px < 0) { px += WINDOW_WIDTH; }
-//        if (px >= WINDOW_WIDTH) { px -= WINDOW_WIDTH; }
-//        if (py < 0) { py += WINDOW_HEIGHT; }
-//        if (py >= WINDOW_HEIGHT) { py -= WINDOW_HEIGHT; }
-        
         
         // collision with window borders
         if ((px - c->getRadius() - 1) < 0) { px += 1; vx = -vx; }
         if ((px + c->getRadius() + 1) > WINDOW_WIDTH) { px -= 1; vx = -vx;}
         if ((py - c->getRadius() - 1) < 0) { py += 1; vy = -vy; }
         if ((py + c->getRadius() + 1) > WINDOW_HEIGHT) { py -= 1; vy = -vy;}
+        
+        
+        auto isCollision = [](float x1, float y1, float r1, float x2, float y2, float r2) {
+            float dx = x1 - x2;
+            float dy = y1 - y2;
+            float dist = sqrt(dx * dx + dy * dy);
+            float rad_dist = r1 + r2;
+            return (dist <= rad_dist);
+        };
+        std::vector<std::pair<Ball*, Ball*>> collisions;
+        for (int i = 0; i < circles.size(); ++i) {
+            if (&circles[i] != this) {
+                if (isCollision(circles[i].px, circles[i].py, circles[i].c->getRadius(), px, py, c->getRadius())) {
+                    
+                    collisions.push_back({&circles[i], this});
+                    
+                    float fDist = sqrtf((circles[i].px - px)*(circles[i].px - px) + (circles[i].py - py)*(circles[i].py - py));
+                    float fOverlap = 0.5f * (fDist - circles[i].c->getRadius() - c->getRadius());
+                    
+                    circles[i].px -= fOverlap * (circles[i].px - px) / fDist;
+                    circles[i].py -= fOverlap * (circles[i].py - py) / fDist;
+                    px += fOverlap * (circles[i].px - px) / fDist;
+                    py += fOverlap * (circles[i].py - py) / fDist;
+                }
+            }
+        }
+        //collision detection between circles
+        for (auto &collision : collisions) {
+            auto& a = *(collision.first);
+            auto& b = *(collision.second);
+            if (&(a.c) != &(b.c)) {
+                    float fDist = sqrtf((a.px - b.px)*(a.px - b.px) + (a.py - b.py)*(a.py - b.py));
+                
+                    float nx = (b.px - a.px) / fDist;
+                    float ny = (b.py - a.py) / fDist;
+                
+                    float tx = -ny;
+                    float ty = nx;
+                
+                    float dpTan1 = a.vx * tx + a.vy * ty;
+                    float dpTan2 = b.vx * tx + b.vy * ty;
+                
+                    float dpNorm1 = a.vx * nx + a.vy * ny;
+                    float dpNorm2 = b.vx * nx + b.vy * ny;
+                
+                    float m1 = (dpNorm1 * (a.mass - b.mass) + 2.0f * b.mass * dpNorm2) / (a.mass + b.mass);
+                    float m2 = (dpNorm2 * (b.mass - a.mass) + 2.0f * a.mass * dpNorm1) / (a.mass + b.mass);
+                
+                    a.vx = tx * dpTan1 + nx * m1;
+                    a.vy = ty * dpTan1 + ny * m1;
+                    b.vx = tx * dpTan2 + nx * m2;
+                    b.vy = ty * dpTan2 + ny * m2;
+                }
+            }
     }
-    
 };
 
 DemoModeSc::DemoModeSc() { }
@@ -81,7 +127,7 @@ int DemoModeSc::Run(sf::RenderWindow &App) {
         Ball b;
         circles.push_back(b);
     }
-    
+    sf::Clock clock;
     while(running) {
         
         while(App.pollEvent(event)) {
@@ -93,6 +139,7 @@ int DemoModeSc::Run(sf::RenderWindow &App) {
                 case sf::Event::Closed: {
                     return (-1);
                     break;
+                default: break;
                 }
                     
             }
@@ -101,7 +148,7 @@ int DemoModeSc::Run(sf::RenderWindow &App) {
        
         App.clear(sf::Color::Black);
         for (int i = 0; i < amount_of_circles; ++i) {
-            circles[i].update();
+            circles[i].update(circles, clock.restart().asMilliseconds());
         }
         for (int i = 0; i < amount_of_circles; ++i) {
             App.draw(circles[i]);
